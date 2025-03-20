917a7645a72c426c88564b7322e2cc15450ff35c
KingDor123 <145506079+KingDor123@users.noreply.github.com>
2025-03-20 12:45:48 +0200

    הוספת הדפסה נוספת לפונקציה״

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 def add_numbers(a, b):
+    print("This is a simple addition function")
     return a + b
+
 def multiply_numbers(a, b):
     return a * b
+
 
 if __name__ == "__main__":
     print(f"3 + 5 = {add_numbers(3, 5)}")
